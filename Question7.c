ded4b43448d32ec13f805cfc8ff7e2e55ad9cf47
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-14 09:38:02 +0530

    Update Question7.c

--- Question7.c
+++ Question7.c
@@ -1,3 +1,14 @@
+/*
+   Write a C program to calculate the distance between the two points. 
+ 
+    Test Data : 
+    Input x1: 25 
+    Input y1: 15 
+    Input x2: 35 
+    Input y2: 10 
+    Expected Output: 
+    Distance between the said points: 11.1803
+*/
 #include<stdio.h>
 #include<math.h>
 int main(){
