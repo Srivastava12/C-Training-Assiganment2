56a8712b151f690f11043e226a311fa96b06d564
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-14 09:36:31 +0530

    Update Question5.c

--- Question5.c
+++ Question5.c
@@ -1,3 +1,16 @@
+/*
+   Write a C program that accepts two item’s weight (floating points' values ) and number of 
+   purchase (floating points' values) and calculate the average value of the items. 
+ 
+    Test Data : 
+    Weight - Item1: 15 
+    No. of item1: 5 
+    Weight - Item2: 25 
+    No. of item2: 4 
+    Expected Output: 
+    Average Value = 19.444444 
+
+*/
 #include<stdio.h>
 int main(){
 	//Variable Declarating
