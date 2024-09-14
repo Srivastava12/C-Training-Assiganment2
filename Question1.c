eb57ba40166a44178fe5a715614339b6617f1276
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-14 09:32:49 +0530

    Update Question1.c

--- Question1.c
+++ Question1.c
@@ -1,3 +1,18 @@
+/* C program to calculate Compound Interest 
+ 
+    Example Input 
+    Enter principle (amount): 1200 
+    Enter time: 2 
+    Enter rate: 5.4 
+    Output 
+ 
+    Compound Interest = 1333.099243 
+ 
+    Compound Interest formula 
+    Formula to calculate compound interest annually is given by. 
+ 
+    CI = P(1 + R/100)^T
+*/
 #include<stdio.h>
 #include<math.h>
 int main(){
