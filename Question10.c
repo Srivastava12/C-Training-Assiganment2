4e6c9887f6edd71bf363fa15bcfa662bc81b865d
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-14 09:40:27 +0530

    Update Question10.c

--- Question10.c
+++ Question10.c
@@ -1,3 +1,7 @@
+/*
+  Write a C program to input principle, time and rate (P, T, R) from user and find Simple Interest. 
+  How to calculate simple interest in C programming.
+*/
 #include<stdio.h>
 int main(){
 	//Variable Declarating
