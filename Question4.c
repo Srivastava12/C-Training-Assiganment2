c0a47f22d8c8433afb20088aeb27e5230ea3de92
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-14 09:35:31 +0530

    Update Question4.c

--- Question4.c
+++ Question4.c
@@ -1,3 +1,10 @@
+/*
+   Write a C program to print the following characters in a reverse way. 
+    Test Characters: 'X', 'M', 'L' 
+ 
+    Expected Output: 
+    The reverse of XML is LMX
+*/
 #include<stdio.h>
 #include<string.h>
 int main(){
