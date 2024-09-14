1be7bd7b1072f2f2268b4d5fe5fd71bb7a0349ec
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-14 09:34:32 +0530

    Update Question3.c

--- Question3.c
+++ Question3.c
@@ -1,3 +1,14 @@
+/*
+C program to find area of an equilateral triangle 
+    Example Input 
+ 
+ Enter side of the equilateral triangle: 10 
+ Output 
+ 
+ Area of equilateral triangle = 43.3 sq. units 
+ 
+ C equivalent expression to find area of equilateral triangle -  (sqrt(3) / 4) * (side * side)
+*/
 #include<stdio.h>
 #include<math.h>
 int main(){
